0e1dc84e87748e49b1b5afa31aa080fa672e8c69
Jonathan Delgado <hi@jon.soy>
2023-10-22 13:34:21 -0300

    refactor: remove code unused

--- src/db/models.ts
+++ src/db/models.ts
@@ -23,6 +23,8 @@ export class Models {
   }
 }
 
+export type LogStreamManifest = ObjFromSchema<typeof LogStreamManifestSchema>
+
 export const MessageSchema = Schema.createSchema({
   id: SchemaTypes.string,
   message: SchemaTypes.string,
@@ -47,62 +49,3 @@ export const LogStreamManifestSchema = Schema.createSchema({
 }, {
   indexes: ['name']
 })
-
-/** @deprecated */
-export interface RefConnection {
-  instances: Set<Symbol>
-  connection: Promise<Connection>
-  destroy(): Promise<void>
-}
-
-/** @deprecated */
-export interface SubscriptionRefConnection {
-  connection: Promise<Connection>
-  unsubscribe(): Promise<void>
-}
-const mapRefConnections = new Map<string, RefConnection>();
-
-/** @deprecated */
-export const selectRefConnection = (nameConnection: string): RefConnection => {
-  const refConnection = mapRefConnections.get(nameConnection)
-  if (refConnection) return refConnection
-  const connection = ConnectionFactory.create(nameConnection)
-  const nextRefConnection: RefConnection = {
-    instances: new Set(),
-    connection: ConnectionFactory.create(nameConnection),
-    async destroy() {
-      const conn = await connection;
-      await conn.close();
-    }
-  }
-  mapRefConnections.set(nameConnection, nextRefConnection)
-  return nextRefConnection
-}
-
-/** @deprecated */
-export const subscribeConnection = (nameConnection: string): SubscriptionRefConnection => {
-  const indexInterface = Symbol('instance')
-  const refConnection = selectRefConnection(nameConnection)
-
-  refConnection.instances.add(indexInterface)
-
-  return {
-    connection: refConnection.connection,
-    async unsubscribe() {
-      refConnection.instances.delete(indexInterface)
-      if (refConnection.instances.size === 0) {
-        refConnection.destroy()
-      }
-    }
-  }
-}
-
-/** @deprecated */
-export const createLogStreamModel = async (subscriptionRefConnection: SubscriptionRefConnection, LogsStreamName: string) => {
-  return await Model.create(LogsStreamName, MessageSchema, await subscriptionRefConnection.connection)
-}
-
-/** @deprecated */
-export const createMetaLogStreamModel = async (subscriptionRefConnection: SubscriptionRefConnection, modelName: string) => {
-  return await Model.create(modelName, LogStreamManifestSchema, await subscriptionRefConnection.connection)
-}
